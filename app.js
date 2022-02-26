f03d1f4c178d826e0326cc3489ff54752893ebb1
Ashu190301 <ashu190301@gmail.com>
2022-02-26 21:52:28 +0530

    PORT CHANGE

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const https = require("https");
 const app = express();
 const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
 
  
 app.use(bodyParser.urlencoded({extended: true}));
@@ -42,6 +43,6 @@ app.post("/", function(req,res){
 }) 
 
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
   console.log("Server is running on port 3000");
 })
